8f6899195246c5e29861763c7649742cfef6530a
marie-mccarthy <marie.mccarthy2000@gmail.com>
2016-09-07 08:02:18 -0600

    not finished, but working on it

--- ArrayPractice/src/ArrayPractice1.java
+++ ArrayPractice/src/ArrayPractice1.java
@@ -2,25 +2,48 @@ import java.util.Scanner;
 
 public class ArrayPractice1
 	{
+	static int counter = 0;
 		public static void main(String[] args)
 		{
-			exercise1();			
+			exercise1();		
+			exercise2();
 		}
 	public static int exercise1()
 	{
-		System.out.println("How many favorite sweets do you have");
 		Scanner userInput2= new Scanner(System.in);
 		int number = userInput2.nextInt();	
-			
-		System.out.println("What are they");
-		Scanner userInput= new Scanner(System.in);
-		String sweetName = userInput.nextLine();
 		
 		String[] sweetsList = new String[number];
-		sweetsList[number] = sweetName;
-			
-		System.out.println(sweetsList);
-		return number;
-	}
 		
-}
+		
+		for(String sweets:sweetsList)
+		{
+			System.out.println("enter a sweet please:");
+			Scanner userInput= new Scanner(System.in);
+			String sweetName = userInput.nextLine();
+			System.out.println(sweetName);
+		}
+		for(String sweets:sweetsList)
+		{
+		//	System.out.println(sweetName);
+		}
+	return number;
+	}
+	public static int exercise2()
+	{
+		System.out.println("Please input 8 numbers");
+		int[] myArray = new int[8];
+	
+		for(int numbers:myArray)
+		{
+			System.out.println("enter a number:");
+			Scanner userInput= new Scanner(System.in);
+			int numbers = userInput.nextInt();
+			myArray[8]=numbers;
+		}
+
+	{
+	}
+	}
+	}
+
